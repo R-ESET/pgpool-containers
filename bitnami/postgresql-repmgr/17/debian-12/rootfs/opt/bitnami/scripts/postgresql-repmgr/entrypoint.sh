33fc6ffd6340c1786876dfdc2515c60d8836a556
Megha-Vecto <megha.shelare@vectotech.in>
2025-09-10 12:29:54 +0530

    reverted entrypoint and docker

--- bitnami/postgresql-repmgr/17/debian-12/rootfs/opt/bitnami/scripts/postgresql-repmgr/entrypoint.sh
+++ bitnami/postgresql-repmgr/17/debian-12/rootfs/opt/bitnami/scripts/postgresql-repmgr/entrypoint.sh
@@ -31,50 +31,10 @@ debug "Copying files from $POSTGRESQL_DEFAULT_CONF_DIR to $POSTGRESQL_CONF_DIR"
 cp -nr "$POSTGRESQL_DEFAULT_CONF_DIR"/. "$POSTGRESQL_CONF_DIR"
 
 if [[ "$*" = *"/opt/bitnami/scripts/postgresql-repmgr/run.sh"* ]]; then
-    if [ ! -f "$POSTGRESQL_TMP_DIR/.initialized" ]; then
-
-        info "** First run detected:Starting PostgreSQL with Replication Manager setup **"
-         # /opt/bitnami/scripts/postgresql-repmgr/setup.sh
-        touch "$POSTGRESQL_TMP_DIR"/.initialized
-        info "** PostgreSQL with Replication Manager setup finished! **"
-    else
-      info "** Existing cluster detected: skipping setup.sh **"
-  fi
-fi
-
-PGDATA="${POSTGRESQL_DATA_DIR:-/bitnami/postgresql/data}"
-CONF_SRC="/opt/bitnami/postgresql/conf"
-
-# If pg_hba.conf is missing completely → copy defaults
-if [ ! -f "$PGDATA/pg_hba.conf" ]; then
-  echo "ℹ️  pg_hba.conf missing in $PGDATA, copying from $CONF_SRC"
-  cp "$CONF_SRC/pg_hba.conf" "$PGDATA/"
-  cp "$CONF_SRC/postgresql.conf" "$PGDATA/"
-  chown 1001:1001 "$PGDATA/"*.conf
-  chmod 640 "$PGDATA/"*.conf
-fi
-
-# Now patch pg_hba.conf to point to a relative hba.d
-if [ -f "$PGDATA/postgresql.conf" ]; then
-  echo "ℹ️  Ensuring postgresql.conf has include_dir for hba.d"
-  if ! grep -q "include_dir = 'hba.d'" "$PGDATA/postgresql.conf"; then
-    echo "include_dir = 'hba.d'" >> "$PGDATA/postgresql.conf"
-  fi
-
-
-  mkdir -p "$PGDATA/hba.d"
-  chown -R 1001:1001 "$PGDATA/hba.d"
-  chmod 750 "$PGDATA/hba.d"
-
-  # Inject a minimal rule so it can start
-cat > "$PGDATA/hba.d/00-local.conf" <<'EOF'
-local   all             all                                     trust
-host    all             all             127.0.0.1/32            md5
-host    all             all             ::1/128                 md5
-host    replication     repmgr          0.0.0.0/0               md5
-EOF
-  chown 1001:1001 "$PGDATA/hba.d/00-local.conf"
-  chmod 640 "$PGDATA/hba.d/00-local.conf"
+    info "** Starting PostgreSQL with Replication Manager setup **"
+    /opt/bitnami/scripts/postgresql-repmgr/setup.sh
+    touch "$POSTGRESQL_TMP_DIR"/.initialized
+    info "** PostgreSQL with Replication Manager setup finished! **"
 fi
 
 echo ""
